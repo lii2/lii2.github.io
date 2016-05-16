d278a839075a0d626a897e5e06915696b745975f
lii2 <lii.jeffrey3@gmail.com>
2016-05-15 23:01:09 -0400

    Finished projects page

--- scripts/myScript.js
+++ scripts/myScript.js
@@ -3,10 +3,4 @@
 window.onload = function () {
     var selfpic = document.getElementById('selfpic');
     selfpic.className -= "fadein";
-
 };
-
-function displayEportfolioInfo() {
-    var projectparagraph = document.getElementById('projectparagraph');
-    projectparagraph.src = "images/eportfolioparagraph.png";
-}
